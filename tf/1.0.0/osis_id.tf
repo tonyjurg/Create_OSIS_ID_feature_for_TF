a92ca3b8a4d46690822552b97b293be674982594
tonyjurg <github@jurgnet.nl>
2025-05-26 17:11:08 +0200

    align license

--- tf/1.0.0/osis_id.tf
+++ tf/1.0.0/osis_id.tf
@@ -1,17 +1,17 @@
 @node
-@author=OSIS initiative
+@author=OSIS (Open Scriptural Information Standard) initiative
 @convertedBy=Tony Jurg
 @coreData=Nestle 1904 Text-Fabric (centerBLC)
 @coreDataUrl=https://github.com/CenterBLC/N1904
 @description=Book name according to the OSIS (Open Scriptural Information Standard) ID.
-@license=MIT License
-@licenseUrl=https://mit-license.org/
+@license=Creative Commons Attribution 4.0 International (CC BY 4.0)
+@licenseUrl=https://github.com/tonyjurg/N1904addons/blob/main/LICENSE.md
 @provenance=jupyter Notebook (https://github.com/tonyjurg/create_TF_feature_betacode)
 @valueType=str
 @version=1.0.0
 @website=https://github.com/tonyjurg/N1904addon
 @writtenBy=Text-Fabric
-@dateWritten=2025-05-25T18:15:49+00:00Z
+@dateWritten=2025-05-26T15:07:36+00:00Z
 
 137780	Matt
 Mark
